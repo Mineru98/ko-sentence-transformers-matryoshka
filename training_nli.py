e595e81a5ec6af295217a6a33c08c19133e230da
iamiks <rmstjr1030@naver.com>
2024-02-24 01:01:33 +0900

    Feat : Apply new Loss Function(MatryoshkaLoss, CoSENTLoss) in NIL dataset

--- training_nli.py
+++ training_nli.py
@@ -15,7 +15,14 @@ from datetime import datetime
 
 import numpy as np
 import torch
-from sentence_transformers import SentenceTransformer, LoggingHandler, models, losses, datasets
+from sentence_transformers import (
+    SentenceTransformer,
+    LoggingHandler,
+    models,
+    losses,
+    datasets,
+)
+from sentence_transformers.losses import CoSENTLoss, MatryoshkaLoss
 from sentence_transformers.evaluation import EmbeddingSimilarityEvaluator
 
 from data_util import load_kor_sts_samples, load_kor_nli_samples
@@ -39,53 +46,79 @@ torch.cuda.manual_seed(args.seed)
 
 # Configure logger
 logging.basicConfig(
-    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, handlers=[LoggingHandler()]
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
 )
 
 # Read the dataset
 model_save_path = os.path.join(
     args.output_dir,
-    args.output_prefix + args.model_name_or_path.replace("/", "-") + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    args.output_prefix
+    + args.model_name_or_path.replace("/", "-")
+    + "-"
+    + datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
 )
 
 # Define SentenceTransformer model
-word_embedding_model = models.Transformer(args.model_name_or_path, max_seq_length=args.max_seq_length)
-pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-                               pooling_mode_mean_tokens=True,
-                               pooling_mode_cls_token=False,
-                               pooling_mode_max_tokens=False)
+word_embedding_model = models.Transformer(
+    args.model_name_or_path, max_seq_length=args.max_seq_length
+)
+pooling_model = models.Pooling(
+    word_embedding_model.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True,
+    pooling_mode_cls_token=False,
+    pooling_mode_max_tokens=False,
+)
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 # Read the dataset
-nli_dataset_path = 'KorNLUDatasets/KorNLI'
-sts_dataset_path = 'KorNLUDatasets/KorSTS'
+nli_dataset_path = "KorNLUDatasets/KorNLI"
+sts_dataset_path = "KorNLUDatasets/KorSTS"
 logging.info("Read KorNLI train/KorSTS dev dataset")
 train_files = glob.glob(os.path.join(nli_dataset_path, "*train.ko.tsv"))
 dev_file = os.path.join(sts_dataset_path, "sts-dev.tsv")
 train_samples = []
 for train_file in train_files:
     train_samples += load_kor_nli_samples(train_file)
-train_dataloader = datasets.NoDuplicatesDataLoader(train_samples, batch_size=args.batch_size)
+train_dataloader = datasets.NoDuplicatesDataLoader(
+    train_samples, batch_size=args.batch_size
+)
 dev_samples = load_kor_sts_samples(dev_file)
-dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, batch_size=args.batch_size, name='sts-dev')
-train_loss = losses.MultipleNegativesRankingLoss(model)
+dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    dev_samples, batch_size=args.batch_size, name="sts-dev"
+)
+base_loss = CoSENTLoss(model=model)
+train_loss = MatryoshkaLoss(
+    model=model,
+    loss=base_loss,
+    matryoshka_dims=[768, 512, 256, 128, 64],
+    matryoshka_weight=[1, 1, 1, 1, 1],
+)
 
 # Configure the training.
-warmup_steps = math.ceil(len(train_dataloader) * args.num_epochs * 0.1)  # 10% of train data for warm-up
+warmup_steps = math.ceil(
+    len(train_dataloader) * args.num_epochs * 0.1
+)  # 10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
 
 # Train the model
-model.fit(train_objectives=[(train_dataloader, train_loss)],
-          evaluator=dev_evaluator,
-          epochs=args.num_epochs,
-          evaluation_steps=1000,
-          warmup_steps=warmup_steps,
-          output_path=model_save_path)
+model.fit(
+    train_objectives=[(train_dataloader, train_loss)],
+    evaluator=dev_evaluator,
+    epochs=args.num_epochs,
+    evaluation_steps=1000,
+    warmup_steps=warmup_steps,
+    output_path=model_save_path,
+)
 
 # Load the stored model and evaluate its performance on STS benchmark dataset
 model = SentenceTransformer(model_save_path)
 logging.info("Read KorSTS benchmark test dataset")
 test_file = os.path.join(sts_dataset_path, "sts-test.tsv")
 test_samples = load_kor_sts_samples(test_file)
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    test_samples, name="sts-test"
+)
 test_evaluator(model, output_path=model_save_path)
--- training_sts.py
+++ training_sts.py
@@ -38,49 +38,72 @@ torch.cuda.manual_seed(args.seed)
 
 # Configure logger
 logging.basicConfig(
-    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO, handlers=[LoggingHandler()]
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    handlers=[LoggingHandler()],
 )
 
 # Read the dataset
 model_save_path = os.path.join(
     args.output_dir,
-    args.output_prefix + args.model_name_or_path.replace("/", "-") + '-' + datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    args.output_prefix
+    + args.model_name_or_path.replace("/", "-")
+    + "-"
+    + datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
 )
 
 # Define SentenceTransformer model
-word_embedding_model = models.Transformer(args.model_name_or_path, max_seq_length=args.max_seq_length)
-pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(),
-                               pooling_mode_mean_tokens=True,
-                               pooling_mode_cls_token=False,
-                               pooling_mode_max_tokens=False)
+word_embedding_model = models.Transformer(
+    args.model_name_or_path, max_seq_length=args.max_seq_length
+)
+pooling_model = models.Pooling(
+    word_embedding_model.get_word_embedding_dimension(),
+    pooling_mode_mean_tokens=True,
+    pooling_mode_cls_token=False,
+    pooling_mode_max_tokens=False,
+)
 model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
 
 # Read the dataset
 logging.info("Read KorSTS train/dev dataset")
-sts_dataset_path = 'KorNLUDatasets/KorSTS'
-train_file, dev_file = os.path.join(sts_dataset_path, "sts-train.tsv"), os.path.join(sts_dataset_path, "sts-dev.tsv")
-train_samples, dev_samples = load_kor_sts_samples(train_file), load_kor_sts_samples(dev_file)
+sts_dataset_path = "KorNLUDatasets/KorSTS"
+train_file, dev_file = (
+    os.path.join(sts_dataset_path, "sts-train.tsv"),
+    os.path.join(sts_dataset_path, "sts-dev.tsv"),
+)
+train_samples, dev_samples = (
+    load_kor_sts_samples(train_file),
+    load_kor_sts_samples(dev_file),
+)
 train_dataloader = DataLoader(train_samples, shuffle=True, batch_size=args.batch_size)
-dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(dev_samples, batch_size=args.batch_size,
-                                                                 name='sts-dev')
+dev_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    dev_samples, batch_size=args.batch_size, name="sts-dev"
+)
 train_loss = losses.CosineSimilarityLoss(model=model)
 
 # Configure the training.
-warmup_steps = math.ceil(len(train_dataloader) * args.num_epochs * 0.1)  # 10% of train data for warm-up
+warmup_steps = math.ceil(
+    len(train_dataloader) * args.num_epochs * 0.1
+)  # 10% of train data for warm-up
 logging.info("Warmup-steps: {}".format(warmup_steps))
 
 # Train the model
-model.fit(train_objectives=[(train_dataloader, train_loss)],
-          evaluator=dev_evaluator,
-          epochs=args.num_epochs,
-          evaluation_steps=1000,
-          warmup_steps=warmup_steps,
-          output_path=model_save_path)
+model.fit(
+    train_objectives=[(train_dataloader, train_loss)],
+    evaluator=dev_evaluator,
+    epochs=args.num_epochs,
+    evaluation_steps=1000,
+    warmup_steps=warmup_steps,
+    output_path=model_save_path,
+)
 
 # Load the stored model and evaluate its performance on STS benchmark dataset
 model = SentenceTransformer(model_save_path)
 logging.info("Read KorSTS benchmark test dataset")
 test_file = os.path.join(sts_dataset_path, "sts-test.tsv")
 test_samples = load_kor_sts_samples(test_file)
-test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(test_samples, name='sts-test')
+test_evaluator = EmbeddingSimilarityEvaluator.from_input_examples(
+    test_samples, name="sts-test"
+)
 test_evaluator(model, output_path=model_save_path)
